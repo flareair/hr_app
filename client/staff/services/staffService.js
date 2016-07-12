28cde89f9cecd9c844d8e21f89758885bf2e8cb9
flareair <flareair0@gmail.com>
2016-07-12 17:32:39 +0300

    Add inmemory caching

--- client/staff/services/staffService.js
+++ client/staff/services/staffService.js
@@ -1,15 +1,36 @@
 'use strict';
 
 export default class boardService {
-    constructor($http)  {
+    constructor($http, $q)  {
         this.$http = $http;
+        this.$q = $q;
         // basic twitch api url
         this.url = '/api/staff/all';
+        this.staff = [];
     }
 
     getAllScammers() {
-        return this.$http.get(this.url);
+
+        // very simple inmemory caching
+
+        if (this.staff.length > 0) {
+            console.log('from cache!');
+
+            let deferred = this.$q.defer();
+
+            deferred.resolve({
+                data: this.staff
+            });
+
+            return deferred.promise;
+        }
+
+        return this.$http.get(this.url)
+            .then((res) => {
+                this.staff = res.data;
+                return res;
+            });
     }
 }
 
-boardService.$inject = ['$http'];
+boardService.$inject = ['$http', '$q'];
