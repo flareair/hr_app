02964e98cc5ea27ba7dabdb77affd7115942cdfd
flareair <flareair0@gmail.com>
2016-07-13 13:38:44 +0300

    Add mock staff data for e2e testing

--- server/api/apiController.js
+++ server/api/apiController.js
@@ -1,13 +1,27 @@
 'use strict';
 const fs = require('fs');
 
-let data = require('../data.json');
+let data = '';
+
+if (process.env.NODE_ENV === 'test') {
+    data = require('../test_data.json');
+} else {
+    data = require('../data.json');
+}
+
+
 
 exports.defaultInfo = (req, res, next) => {
     res.end('Welcome to GAS Club Board Of Shame api');
 };
 
 exports.getAll = (req, res, next) => {
+    if (!data) {
+        let err = new Error('Cant get mock data');
+        err.status = 500;
+        return next(err);
+    }
+
     return res.send(data);
 };
 
